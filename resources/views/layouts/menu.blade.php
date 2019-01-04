40f1cbabc4600dbc18efccc3787c7773560e7ef5
TopEpp <natchapol.prms@gmail.com>
2019-01-04 14:41:05 +0700

    edit lang

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,29 +1,28 @@
 <li class="{{ Request::is('products*') ? 'active' : '' }}">
-    <a href="{!! route('products.index') !!}"><i class="fa fa-edit"></i><span>Products</span></a>
+    <a href="{!! route('products.index') !!}"><i class="fa fa-edit"></i><span>สินค้า</span></a>
 </li>
 
 <li class="{{ Request::is('categories*') ? 'active' : '' }}">
-    <a href="{!! route('categories.index') !!}"><i class="fa fa-edit"></i><span>Categories</span></a>
+    <a href="{!! route('categories.index') !!}"><i class="fa fa-edit"></i><span>ประเภทสินค้า</span></a>
 </li>
 
 
 <li class="{{ Request::is('stocks*') ? 'active' : '' }}">
-    <a href="{!! route('stocks.index') !!}"><i class="fa fa-edit"></i><span>Stocks</span></a>
+    <a href="{!! route('stocks.index') !!}"><i class="fa fa-edit"></i><span>คลังสินค้า</span></a>
 </li>
 
 <li class="{{ Request::is('imports*') ? 'active' : '' }}">
-    <a href="{!! route('imports.index') !!}"><i class="fa fa-edit"></i><span>Imports</span></a>
+    <a href="{!! route('imports.index') !!}"><i class="fa fa-edit"></i><span>นำเข้าสินค้า</span></a>
 </li>
 
 <li class="{{ Request::is('orders*') ? 'active' : '' }}">
-    <a href="{!! route('orders.index') !!}"><i class="fa fa-edit"></i><span>Orders</span></a>
+    <a href="{!! route('orders.index') !!}"><i class="fa fa-edit"></i><span>สั่งสินค้า</span></a>
 </li>
 
 <li class="{{ Request::is('users*') ? 'active' : '' }}">
-    <a href="{!! route('users.index') !!}"><i class="fa fa-edit"></i><span>Users</span></a>
+    <a href="{!! route('users.index') !!}"><i class="fa fa-edit"></i><span>ผู้ใช้งาน</span></a>
 </li>
 
 <li class="{{ Request::is('units*') ? 'active' : '' }}">
-    <a href="{!! route('units.index') !!}"><i class="fa fa-edit"></i><span>Units</span></a>
+    <a href="{!! route('units.index') !!}"><i class="fa fa-edit"></i><span>หน่วยนับ</span></a>
 </li>
-
